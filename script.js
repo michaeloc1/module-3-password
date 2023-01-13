9921737f50f12df7d949ad80d5d7cb51825c7c74
michaeloc1 <michaeloc@verizon.net>
2023-01-13 08:46:51 -0500

    changed validation

--- script.js
+++ script.js
@@ -5,26 +5,33 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   //give user prompt asking for password length
   let passLength = prompt("Choose password length: Must be between 8 and 128");
- //validate passLength continue if ok continue if false resubmit prompt
- while (isNaN(passLength) || passLength === null || passLength < 8 || passLength > 128){
-  passLength = prompt("Choose password length: Must be numeric and between 8 and 128");
-}
-//prompt user of character types to use
-var useLower = confirm("Do you want to use lowercase letters");
-var useUpper = confirm("Do you want to use uppercase letters");
-var useNums = confirm("Do you want to use numbers");
-var useSpecial = confirm("Do you want to include special characters");
+  //validate passLength continue if ok continue if false break
+  if (isNaN(passLength) || passLength === null){
+    confirm("You must enter a valid number");
+    return;
+  }
+  else if( passLength < 8){
+    confirm("Length must be at least 8");
+    return;
+  }
+  else if(passLength > 128){
+    confirm("Length must be less than 129");
+    return;
+  }
+ 
+  //prompt user of character types to use
+  var useLower = confirm("Do you want to use lowercase letters");
+  var useUpper = confirm("Do you want to use uppercase letters");
+  var useNums = confirm("Do you want to use numbers");
+  var useSpecial = confirm("Do you want to include special characters");
 
-//validate at least on character type chosen if not resubmit
+  //validate at least on character type chosen if not return
+  if(!useLower && !useUpper && !useNums && !useSpecial){
+    confirm("You must choose at least one character type");
+    return;
+  }
 
-while (!useLower && !useUpper && !useNums && !useSpecial){
-confirm("You must choose at least one character type")
-useLower = confirm("Do you want to use lowercase letters");
-useUpper = confirm("Do you want to use uppercase letters");
-useNums = confirm("Do you want to use numbers");
-useSpecial = confirm("Do you want to include special characters");
-}
-var toUseCharArr = [useLower, useUpper, useNums, useSpecial]
+  var toUseCharArr = [useLower, useUpper, useNums, useSpecial]
   var password = generatePassword(passLength, toUseCharArr);
   var passwordText = document.querySelector("#password");
 
@@ -37,39 +44,39 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-function generatePassword(length, charsArr){
+function generatePassword(length, charsArr) {
   var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ]
+  var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",]
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
   var newArr = [];
   //var newArr2 = [];
   var strPassword = "";
 
-  for(var i = 0; i < charsArr.length; i++){
-    if(charsArr[i]){
-      if( i === 0){
+  for (var i = 0; i < charsArr.length; i++) {
+    if (charsArr[i]) {
+      if (i === 0) {
         newArr.push(lowerCaseLetters);
       }
-      else if(i === 1){
+      else if (i === 1) {
         newArr.push(upperCaseLetters)
-     
+
       }
-      else if(i === 2){
+      else if (i === 2) {
         newArr.push(numbers)
       }
-      else if(i === 3){
+      else if (i === 3) {
         newArr.push(specialChars)
       }
       //console.log(newArr)
     }
-    
+
 
   }
   //console.log(newArr)
-  for(i = 0; i < length; i++){
+  for (i = 0; i < length; i++) {
     let newArr2 = [];
-    newArr2 = newArr[ Math.floor(Math.random() * newArr.length)];
+    newArr2 = newArr[Math.floor(Math.random() * newArr.length)];
     strPassword = strPassword + newArr2[Math.floor(Math.random() * newArr2.length)];
     console.log(strPassword)
   }
